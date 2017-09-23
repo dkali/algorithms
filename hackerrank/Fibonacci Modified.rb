3ce0aabd1957388af35728af3daa8984f6e075b1
Дима Калинников <qix2004@mail.ru>
2017-09-23 08:41:54 +0300

    commit

--- hackerrank/Fibonacci Modified.rb
+++ hackerrank/Fibonacci Modified.rb
@@ -1,8 +1,8 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-t1 = 0, t2 = 0, n = 0
+t1, t2, n = 0, 0, 0
 ARGF.each {|line| t1, t2, n = line.split(" ")}
-arr = [t1.to_i, t2.to_i]
+t1 = t1.to_i
+t2 = t2.to_i
 for i in 2..n.to_i-1
-    arr << arr[i - 2] + arr[i - 1] * arr[i - 1]
+    t1, t2 = t2, t1 + t2 * t2
 end
-puts arr.last
+puts t2
