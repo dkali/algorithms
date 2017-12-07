3f470b1003d5ca9dfcea62307671f57a525c682f
Dmitry Kalinnikov <qix2004@mail.ru>
2017-12-08 01:28:20 +0300

    commit

---
+++ leetcode/22 Generate Parentheses.cpp
@@ -0,0 +1,56 @@
+// Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+// For example, given n = 3, a solution set is:
+
+// [
+//   "((()))",
+//   "(()())",
+//   "(())()",
+//   "()(())",
+//   "()()()"
+// ]
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+    int pairs_num;
+    
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> vec;
+        pairs_num = n;
+        string str = "";
+        bt(str, vec, n);
+        return vec;
+    }
+    
+    void bt(string str, vector<string> &v, int limit){
+        if (limit == 0){
+            v.push_back(reconstruct_brackets(str));
+        }
+        else{
+            str += '(';
+            bt(str, v, limit - 1);
+            str.pop_back();
+            long brackets_opened = (pairs_num - limit);
+            long brackets_closed = str.size() - brackets_opened;
+            if (str != "" && (brackets_opened > brackets_closed)){
+                str += ')';
+                bt(str, v, limit);
+            }
+        }
+    }
+    
+    string reconstruct_brackets(string str){
+        long count = pairs_num * 2 - str.size();
+        string tail = "";
+        if (count > 0){
+            tail = string(count, ')');
+            str += tail;
+        }
+        return str;
+    }
+};
