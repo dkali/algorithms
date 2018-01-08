ccc2ba4d875f8ae1cb571a4d8b248f1823512f3f
dkali <secretshelter@gmail.com>
2018-01-08 22:21:16 +0300

    commit

--- leetcode/43 Multiply Strings.cpp
+++ leetcode/43 Multiply Strings.cpp
@@ -9,6 +9,47 @@
 // Both num1 and num2 does not contain any leading zero.
 // You must not use any built-in BigInteger library or convert the inputs to integer directly.
 
+
+// ============ reworked, 10 ms ===========
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        long sz1 = num1.size(), sz2 = num2.size();
+        vector<int> product (sz1 + sz2, 0);
+        
+        for (long i = sz1 - 1; i >= 0; i--) {
+            for (long j = sz2 - 1; j >= 0; j--) {
+//                product[sz1 + sz2 - 1 - (sz2 - 1 - j) - (sz1 - 1 - i)]
+                product[i + j + 1] += (num1[i] - '0') * (num2[j] - '0');
+            }
+        }
+        for (long i = sz1 + sz2 - 1; i > 0; i--) {
+            product[i - 1] += product[i] / 10;
+            product[i] = product[i] % 10;
+        }
+        string ret = "";
+        bool skip_zeroes = true;
+        for (long i = 0; i < sz1 + sz2; i++) {
+            if (product[i] != 0 || !skip_zeroes) {
+                skip_zeroes = false;
+                ret += char('0' + product[i]);
+            }
+        }
+        if (ret == "") ret = "0";
+        
+        return ret;
+    }
+};
+
+
+// ============ first try, 25 ms ===========
+
 #include <iostream>
 #include <vector>
 #include <queue>
