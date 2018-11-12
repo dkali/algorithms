c178ea52414df7ab340a6da3cd6c240b147363fe
dkali <secretshelter@gmail.com>
2018-11-13 00:51:56 +0300

    929 update

--- leetcode/929 Unique Email Addresses.cpp
+++ leetcode/929 Unique Email Addresses.cpp
@@ -32,80 +32,51 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <map>
 #include <set>
 
 using namespace std;
 
 class Solution {
 private:
-    vector<string> splitString(string& str, char delimeter) {
-        vector<string> results;
-        string token = "";
-        
-        for (char c : str) {
-            if (c == delimeter && token != "") {
-                results.push_back(token);
-                token = "";
-            }
-            else
-            {
-                token += c;
-            }
-        }
-        if (token != "")
-            results.push_back(token);
-        return results;
-    }
-    
     string process_local_name(string& local_name) {
-        int plus_pos = -1;
-        for (int pos = int(local_name.size() - 1); pos >= 0; pos--) {
-            if (local_name[pos] == '.') {
-                local_name.erase(pos, 1);
-                plus_pos -= 1;
+        string normalized_str = "";
+        bool domain_processed = false;
+        bool ignore_chars = false;
+        
+        for (char c : local_name) {
+            if (c == '@') {
+                domain_processed = true;
             }
-            if (local_name[pos] == '+')
-                plus_pos = pos;
+            
+            if (domain_processed == false) {
+                switch (c) {
+                    case '.':
+                        break;
+                    case '+':
+                        ignore_chars = true;
+                        break;
+                    default:
+                        if (ignore_chars == false)
+                            normalized_str += c;
+                        break;
+                }
+            }
+            else {
+                normalized_str += c;
+            }
         }
         
-        if (plus_pos > 0) {
-            string new_name = "";
-            for (int pos = 0; pos < plus_pos; pos ++)
-                new_name.push_back(local_name[pos]);
-            local_name = new_name;
-        }
-        
-        return local_name;
+        return normalized_str;
     }
     
 public:
     int numUniqueEmails(vector<string>& emails) {
-        map<string, set<string>> sorted_email;
+        set<string> unique_emails;
         
-        //process incoming email addresses
         for (string email : emails) {
-            vector<string> parts = splitString(email, '@');
-            string local_name = parts[0];
-            string doman_name = parts[1];
-            
-            local_name = process_local_name(local_name);
-            
-            if (sorted_email.find(doman_name) == sorted_email.end()) {
-                set<string> domain_emails;
-                domain_emails.insert(local_name);
-                sorted_email[doman_name] = domain_emails;
-            }
-            else{
-                sorted_email[doman_name].insert(local_name);
-            }
+            unique_emails.insert(process_local_name(email));
         }
-
-        //count the resulting unique email adresses
-        int unique = 0;
-        for (auto it = sorted_email.begin(); it != sorted_email.end(); ++it)
-            unique += it->second.size();
-
-        return unique;
+        
+        return (int)unique_emails.size();
     }
 };
