222e2d1c6f275f33f40c28d2ecb0210dabd8f71b
dkali <secretshelter@gmail.com>
2019-11-23 19:23:24 +0300

    709

---
+++ leetcode/709. To Lower Case.cpp
@@ -0,0 +1,29 @@
+// Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+
+// Example 1:
+// Input: "Hello"
+// Output: "hello"
+
+// Example 2:
+// Input: "here"
+// Output: "here"
+
+// Example 3:
+// Input: "LOVELY"
+// Output: "lovely"
+
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    string toLowerCase(string str) {
+        for (int i = 0; i < str.size(); i++) {
+            if (str[i] > 64 && str[i] < 91) {
+                str[i] = str[i] + 32;
+            }
+        }
+        return str;
+    }
+};
