77473ae77f41476eb0eb564c6a03e705351b069e
dkali <secretshelter@gmail.com>
2021-04-25 22:15:59 +0300

    58

---
+++ leetcode/58. Length of Last Word.cpp
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/length-of-last-word/
+
+// Given a string s consists of some words separated by spaces, return the length of the last word in the string. If the last word does not exist, return 0.
+
+// A word is a maximal substring consisting of non-space characters only.
+
+// Example 1:
+// Input: s = "Hello World"
+// Output: 5
+
+// Example 2:
+// Input: s = " "
+// Output: 0
+ 
+// Constraints:
+// 1 <= s.length <= 104
+// s consists of only English letters and spaces ' '.
+
+class Solution {
+public:
+    int lengthOfLastWord(string str) {
+        int count = 0;
+        bool trailingSpace = true;
+        for (long i = str.size() - 1; i >= 0; i--) {
+            if (str[i] == ' ') {
+                if (!trailingSpace)
+                    break;
+            } else {
+                trailingSpace = false;
+                count++;
+            }
+                
+        }
+        
+        return count;
+    }
+};
